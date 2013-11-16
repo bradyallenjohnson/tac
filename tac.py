0597999d5c4696d9b5db510ad26e920c36842520
Brady Johnson <bradyallenjohnson@gmail.com>
2013-11-16 15:34:10 -0500

    another incremental

--- tac.py
+++ tac.py
@@ -7,18 +7,34 @@ CHUNK_SIZE = 1024 # 1 KB chunks by default
 def print_usage():
     print "Usage: tac [-cs chunk size] <filename>"
 
-def tac_complex(fname):
-    # TODO not implemented yet
-    print "ERROR the complex way of doing tac hasnt been implemented yet"
+#
+# this is a more complex way that will be more eficient for larger files
+#
+def tac_complex(fname, fsize):
     # handling files using 'with' will ensure its always properly closed
     with open(fname, 'r') as f:
-        # 0 from beginning, 1 from current position, 2 from the end of the file
-        f.seek(CHUNK_SIZE, 2) # 2 means from the end of the file
-        read_data = f.read(CHUNK_SIZE)
-	# f.tell() returns position in bytes from beginning of file
+        # Iterate backwards through the file
+        for i in range(1, fsize/CHUNK_SIZE):
+            # Set the position in the file to read from
+            # 0 from beginning, 1 from current position, 2 from the end of the file
+            f.seek(CHUNK_SIZE*i, 2)
 
+            read_data = f.read(CHUNK_SIZE)
+            #str.rfind(str, beg=0 end=len(read_data))
+	    # TODO what if a line is longer than a CHUNK_SIZE??
+
+
+        # Check if there are a few bytes left
+        # f.tell() returns position in bytes from beginning of file
+        remaining = f.tell()
+        if remaining != 0:
+            f.seek(0, 0)
+            read_data = f.read(remaining)
+            # TODO finish this
+#
 # this is a simpler way to do it for smaller files
-def tac_simple(fname):
+#
+def tac_simple(fname, fsize):
     contents = []
 
     # handling files using 'with' will ensure its always properly closed
@@ -31,9 +47,9 @@ def tac_simple(fname):
 def tac(fname):
     statinfo = os.stat(fname)
     if statinfo.st_size < SIMPLE_FILESIZE_LIMIT:
-        tac_simple(fname)
+        tac_simple(fname, statinfo.st_size)
     else:
-        tac_complex(fname)
+        tac_complex(fname, statinfo.st_size)
 
 def main():
     if len(sys.argv) < 2:
